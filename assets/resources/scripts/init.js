9e3fa6b45da94a0eda704e5844eb0f975803b70a
alguipires <alguipires@gmail.com>
2021-05-18 23:38:41 -0300

    tste
    teste

---
+++ assets/resources/scripts/init.js
@@ -0,0 +1,35 @@
+﻿(function ($) {
+    $(function () {
+        $("#menu").load("menu.html");
+        $("#footer").load("footer.html");
+        $("#todos-relatorios").load("relatorio-center.html");
+        //$('.sidenav').sidenav();  // ja esta sendo carregado no menu.html com uma tag <script>
+        $('.parallax').parallax();
+
+        $('select').formSelect();
+
+       
+        //inicialização carousel secundario logos
+        $('.carousel').carousel();
+
+        //inicialização carousel principal superior
+        $('.carousel.carousel-slider').carousel({
+            fullWidth: true,
+            indicators: true
+        });
+
+        //set inteval
+        window.setInterval(function () {
+            $('.carousel').carousel('next', 1);
+        }, 4000);
+
+/*
+        //modal opcional
+        $('#modal1').modal();
+        $('#modal1').modal('open');
+
+        $("#input-telefone").mask('(00) 00000-0000', {
+            reverse: false
+        });*/
+    }); // end of document ready
+})(jQuery); // end of jQuery name space
---
+++ assets/resources/scripts/reserva.js
@@ -0,0 +1,9 @@
+(function ($) {
+    $(function () {
+        //data picker
+        $(document).ready(function(){
+            $('.datepicker').datepicker();
+          });
+          
+    }); // end of document ready
+})(jQuery); // end of jQuery name space
